e3961cd81fb3d9e421e03e2f93aac1c4bd4d1284
Naomie <naomie.discala@gmail.com>
2024-07-22 19:03:13 +0900

    updated frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([])
+  const getData = async() => {
+    const res = await axios.get('/api/users')
+    setUsers(res.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map(u => <h4 key={u._id}>userName : {u.userName}</h4>)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
