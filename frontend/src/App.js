fdbf45ecf7dcb09b01ff57d641e1e433c3b9fcbf
Naomie <naomie.discala@gmail.com>
2024-07-31 18:18:49 +0900

    added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import Home from './pages/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Booking from '@pages/Booking';
+import Home from '@pages/Home';
 
-const App = () => {
-
-  return (<Home/>)
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/booking" element={<Booking />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Booking.js
@@ -0,0 +1,12 @@
+const Booking = () => {
+
+   
+
+    return (
+        <main>
+            display agenda
+        </main>
+    )
+}
+
+export default Booking
